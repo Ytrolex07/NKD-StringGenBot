7e114ec1d342011d979d8aceba89b6f9e3caa6eb
Ytrolex07 <ytfarman@proton.me>
2024-05-25 00:16:39 +0530

    Update gen.py

--- StringGen/modules/gen.py
+++ StringGen/modules/gen.py
@@ -231,7 +231,7 @@ async def gen_session(
                 link_preview=False,
                 parse_mode="html",
             )
-            await client(JoinChannelRequest("@nkd_korean_drama_hindi"))
+            await client(JoinChannelRequest("@NKD_Korean_Group"))
         else:
             string_session = await client.export_session_string()
             await client.send_message(
@@ -239,7 +239,7 @@ async def gen_session(
                 txt.format(ty, string_session, SUPPORT_CHAT),
                 disable_web_page_preview=True,
             )
-            await client.join_chat("nkd_korean_drama_hindi")
+            await client.join_chat("NKD_Korean_Group")
     except KeyError:
         pass
     try:
